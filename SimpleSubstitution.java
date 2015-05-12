249339f07e745743c209d97bd40f7d259451f17a
DrAlias <DrAlias@users.noreply.github.com>
2015-05-12 17:45:30 +0000

    added SimpleSubstitution.java

---
+++ SimpleSubstitution.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class SimpleSubstitution {
+  public static void main (String[] args) {
+    /* code */
+  }
+}
+
